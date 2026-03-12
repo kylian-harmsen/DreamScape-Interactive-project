1862d2586d5e9d7a6054eafe20ec2caa59624804
kylian-harmsen <D296238@edu.curio.nl>
2026-03-12 12:01:46 +0100

    project aangmaakt

---
+++ DreamScape Interactive/DreamScape Interactive/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DreamScape_Interactive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
